c471be95a8cf6c24b07ccb776c4d25c8e343b927
Chirag Sethiya <chiragsethiya123@gmail.com>
2020-07-25 16:14:56 +0530

    Booking saved in database

---
+++ models/bookService.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const validator = require("validator")
+
+
+// const serviceSchema = new mongoose.Schema({name:String})
+
+const bookServiceSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        trim : true
+    },
+
+    email:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+  
+    mobile:{
+        type:Number,
+        min : 10
+    },
+
+    address:{
+        type:String
+    },
+
+    city:{
+        type:String
+    },
+
+    state : {
+        type:String
+    },
+
+    typeOfService : {
+        type:String
+    },
+
+    subServices:{
+        type:Array
+    },
+
+    description: {
+        type:String
+    },
+
+    status:{
+        type:String,
+        default:"unappointed",
+        enum: ['appointed',"unappointed",'accepted','rejected'] 
+    },
+    
+
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+
+
+
+const BookService = mongoose.model("BookService", bookServiceSchema)
+module.exports = BookService 
